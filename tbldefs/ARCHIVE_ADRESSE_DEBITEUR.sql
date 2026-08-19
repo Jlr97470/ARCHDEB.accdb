64a0fbf01433dbdc5b18ccf5783e5504a3656890
jlr97442 <royerjeanlaurent@outlook.fr>
2026-08-19 14:11:44 +0400

    Initial commit

---
+++ tbldefs/ARCHIVE_ADRESSE_DEBITEUR.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [ARCHIVE_ADRESSE_DEBITEUR] (
+  [AD_Code_Débiteur] VARCHAR (10),
+  [AD_TypeAdresse_ID] LONG,
+  [AD_GenreComplémentaire] VARCHAR (10),
+  [AD_NomComplémentaire] VARCHAR (64),
+  [AD_Adr1] VARCHAR (50),
+  [AD_Adr2] VARCHAR (120),
+  [AD_Adr3] VARCHAR (50),
+  [AD_Adr4] VARCHAR (50),
+  [AD_Ville] VARCHAR (50),
+  [AD_CodePostal] VARCHAR (10),
+  [AD_Pays] VARCHAR (36),
+  [AD_Tel] VARCHAR (20),
+  [AD_Tel2] VARCHAR (20),
+  [AD_Fax] VARCHAR (20),
+  [AD_email] VARCHAR (50),
+  [AD_Note] VARCHAR (255),
+   CONSTRAINT [PrimaryKey] PRIMARY KEY ([AD_Code_Débiteur], [AD_TypeAdresse_ID])
+)
---
+++ tbldefs/ARCHIVE_ENCAISSEMENT.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [ARCHIVE_ENCAISSEMENT] (
+  [Enc_ID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [Enc_Code_Débiteur] VARCHAR (10),
+  [Enc_Date] DATETIME,
+  [Enc_Type] VARCHAR (5),
+  [Enc_Montant_m1] CURRENCY,
+  [Enc_Montant_m2] CURRENCY,
+  [Enc_Montant_mo] LONG,
+  [Enc_Prin_m1] CURRENCY,
+  [Enc_Prin_m2] CURRENCY,
+  [Enc_Frais_m1] CURRENCY,
+  [Enc_Frais_m2] CURRENCY,
+  [Enc_Type_Paiement] VARCHAR (30),
+  [Enc_Observation] VARCHAR (100),
+  [Enc_Situation] VARCHAR (1),
+  [Enc_FacD_Id] LONG,
+  [Enc_RegD_Id] LONG,
+  [Enc_ERD_Id] LONG
+)
